82aaa92fc2665fee83c8741c2d247dc3861fe94f
vitalicok <vitalijshal@gmail.com>
2016-10-07 16:09:20 +0200

    AddingRefs and Staring Parse

---
+++ Parser/Parser/Model/CompanyAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Model
+{
+    public class CompanyAttributes
+    {
+        public string ProjectName { get; set;}
+        public decimal ReturnedMoney { get; set;}
+    }
+}
